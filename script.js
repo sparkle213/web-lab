4559aba1f7982d581ed9a2a9041afd023566d573
sparkle213 <durvagarg2004@gmail.com>
2026-07-31 15:13:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const openBtn = document.getElementById("openBtn");
+const container = document.querySelector(".container");
+const envelopePage = document.getElementById("envelopePage");
 
 openBtn.addEventListener("click", () => {
-    alert("Button clicked!");
+
+    // Button press effect
+    openBtn.style.transform = "scale(0.95)";
+
+    setTimeout(() => {
+
+        // Fade first page
+        container.classList.add("fade-out");
+
+        setTimeout(() => {
+
+            container.style.display = "none";
+
+            // Show envelope page
+            envelopePage.style.display = "flex";
+
+        }, 800);
+
+    }, 150);
+
 });
